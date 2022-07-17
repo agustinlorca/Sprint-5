b9cb19ff145064a8424a8b21e97e600729c82036
cvidal98 <canduvidal7@gmail.com>
2022-07-17 15:22:36 -0300

    agreago razon tarjetas de credito modificado

--- clases/razon/razon_alta_tarjeta_credito.py
+++ clases/razon/razon_alta_tarjeta_credito.py
@@ -8,7 +8,7 @@ class RazonAltaTarjetaCredito(Razon):
         if not cliente.puede_crear_tarjeta_credito():
             return "Al cliente no se le permite tener tarjeta de credito"
 
-        if cliente.cuenta.total_tarjetas_credito < (evento.totalTarjetasActualmente + 1):
+        if cliente.cuenta.total_tarjetas_credito < (evento.totalTarjetasDeCreditoActualmente + 1):
             return "Ya se tiene el cupo de tarjetas de credito disponibles"
 
         return "No se pudo determinar la razon de por qué no se puede dar de alta tarjeta de credito "
