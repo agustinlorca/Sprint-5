2a429fc1f3ea23be6bf9288ea5ee94adb48d4893
cvidal98 <canduvidal7@gmail.com>
2022-07-17 15:16:32 -0300

    agrego razon tarjeta de credito

---
+++ clases/razon/razon_alta_tarjeta_credito.py
@@ -0,0 +1,14 @@
+from .razon import Razon
+from ..cliente import Cliente
+from ..evento import Evento
+
+
+class RazonAltaTarjetaCredito(Razon):
+    def resolver(self, cliente: Cliente, evento: Evento) -> str:
+        if not cliente.puede_crear_tarjeta_credito():
+            return "Al cliente no se le permite tener tarjeta de credito"
+
+        if cliente.cuenta.total_tarjetas_credito < (evento.totalTarjetasActualmente + 1):
+            return "Ya se tiene el cupo de tarjetas de credito disponibles"
+
+        return "No se pudo determinar la razon de por qué no se puede dar de alta tarjeta de credito "
